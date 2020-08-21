f4003ae1667af46f1c09e9e10da78623bde6de13
Efraim Weiss <efiwiss@gmail.com>
2020-08-21 17:22:51 +0300

    package structure and license

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='auto_struct',
+    version='0.420',
+    packages=['auto_struct', 'auto_struct.data_types', 'auto_struct.data_types.int', 'auto_struct.data_types.array', 'auto_struct.data_types.string'],
+    url='https://github.com/Valmarelox/auto_struct/tree/master',
+    license='MIT',
+    author='efi',
+    author_email='valmarelox@gmail.com',
+    description='Easily parse binary file in python3.8',
+    zip_safe=False
+)
